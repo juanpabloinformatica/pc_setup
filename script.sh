1db4792454d9c79e2c1a3b834035d893512c328e
Juan Pablo Prada Mejia <juanpabloprada02112000@gmail.com>
2024-12-20 21:50:33 +0100

    Still need to work on that

--- script.sh
+++ script.sh
@@ -1,78 +1,57 @@
 #! /usr/bin/bash
-function init(){
-  setPathStartingPoint &&\
-  setPreInstallations &&\
-  installEssentialPackages &&\
-  getConfigFiles &&\
-  setConfigFiles 
+DOTFILE_HOME_CONFIG="home"
+DOTFILE_CONFIG_CONFIG="config"
+DOTFILE_PICTURE_CONFIG="pictures"
+function main(){
+  dotfilesHandler
 }
-function setPreInstallations(){
-  pacman -Sy archlinux-keyring && pacman -Su
+function dotfilesHandler(){
+  getDotfiles
+  # setDotfiles
 }
-function setPathStartingPoint(){
-  cd $HOME
-}
-function installEssentialPackages(){
-echo -e "\n"
-echo -e "STARTING WITH THIS PART OF THE SETUP"
-sudo pacman -S vim pipewire docker git zsh alacritty i3 tmux firefox base-devel wget curl zip unzip stow ripgrep python nodejs vlc &&\
-git clone https://aur.archlinux.org/yay.git $HOME/Downloads &&\
-cd $HOME/Downloads/yay &&\
-makepkg -si &&\
-yay -S simplescreenrecorder &&\
-sudo pacman -S  git zsh alacritty i3 i3bar i3-msg i3lock i3-input i3blocks i3status i3status i3-nagbar i3-dump-log i3-save-tree i3-config-wizard i3-dmenu-desktop i3-sensible-pager i3-sensible-editor i3-sensible-terminal i3-migrate-config-to-v4 tmux firefox base-devel wget curl zip unzip stow ripgrep&&\
-sudo pacman -S bluez bluez-utils feh flameshot htop spotify-launcher valgrind &&\
-sudo pacman -S virtualbox &&\
-sudo gpasswd -a $USER vboxusers &&\
-sudo pacman -S linux-headers &&\
-sudo modprobe vboxdrv &&\
-sudo pacman -S docker && sudo systemctl enable docker.service && sudo usermod -aG docker $USER && newgrp docker &&\
-# installing reflector for handling mirrors
-sudo pacman -S reflector &&\
-sudo systemctl enable reflector.timer &&\
-sudo pacman -S xdg-user-dirs &&\
-exec "$SHELL"&&\
-xdg-user-dirs-update &&\
-echo "FINISHING WITH THIS PART OF THE SETUP"
-echo -e "\n"
-}
-
-function getConfigFiles(){
-  if ! command -v stow 2>&1 >/dev/null
-  then
-    sudo pacman -S stow
+function getDotfiles(){
+ # temporal location 
+ #
+  if [[ ! -d "$HOME/dotfiles" ]]; then
+    git clone https://github.com/juanpabloinformatica/dotfiles.git $HOME/dotfiles
+    local -a dotFileDirFiles=($HOME/dotfiles/*)
+    for dotFile in "${dotFileDirFiles[@]}";do
+      echo "$dotFile"
+      if [[ -d "$dotFile" ]]; then
+        echo "i am a folder : $dotFile"
+        local fileOrFolderName=$(echo "$dotFile##*/")
+        if [[ "$fileOrFolderName" =~ $DOTFILE_HOME_CONFIG ]]; then
+          # createSymlink $dotFile 
+          local -a dotFileHomeConfigs=($dotFile/*)
+          echo "Home ${dotFileHomeConfigs[*]}"
+          for dotFileHomeConfig in "${dotFileHomeConfigs[@]}";do
+            ln -s "$dotFileHomeConfig" $(echo"$HOME/tmp$fileOrFolderName"))
+          done
+        elif [[ $(echo "$dotFile##*/") =~ $DOTFILE_CONFIG_CONFIG ]]; then
+          local -a dotFileConfigConfigs=($dotFile/*)
+          echo "config ${dotFileHomeConfigs[*]}"
+          for dotFileConfigConfig in "${dotFileConfigConfigs[@]}";do
+            ln -s "$dotFileConfigConfig" $(echo"$HOME/tmp/.config$fileOrFolderName")
+          done
+        elif [[ $(echo "$dotFile##*/") =~ $DOTFILE_PICTURE_CONFIG ]]; then
+          local -a dotFilePictureConfigs=($dotFile/*)
+          echo " pictures ${dotFileHomeConfigs[*]}"
+          for dotFilePictureConfig in "${dotFilePictureConfigs[@]}";do
+            ln -s $dotFilePictureConfig $(echo"$HOME/tmp/Pictures$fileOrFolderName")
+          done
+        else
+          echo "Not decided yet"
+        fi
+      # else
+      #   if [[ $(echo "$dotFile##*/") =~  ]]; then
+      #   fi
+      fi
+    done
+  else
+    echo "Folder doesn't exist"
   fi
-  if ! command -v git 2>&1 >/dev/null
-  then
-    sudo pacman -S git
-  fi
-  git clone git@github.com:juanpabloinformatica/dotfiles.git $HOME/Documents/ &&\
-stow --target=$HOME $HOME/Documents/home &&\
-stow --target=$HOME/.config $HOME/Documents/config &&\
-stow --target=$HOME/Pictures $HOME/Documents/Pictures
 }
-
-function setConfigFiles(){
-  setFont &&\
-  setAlacritty &&\
-  setZsh 
+function setDotfiles(){
+  echo "here"
 }
-function setFont(){
-  echo -e "\nINSTALLING Iosevka FONT"
-  mkdir -p $HOME/.local/share/fonts &&\
- wget --directory-prefix $HOME/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Iosevka.zip &&\
-  unzip $HOME/.local/share/fonts/Iosevka.zip -d $HOME/.local/share/fonts/Iosevka.zip &&\
- rm -rf Iosevka.zip
-}
-function setAlacritty(){
-  echo -e "\n GETTING Alacritty THEMES"
-  git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
-}
-function setZsh(){
-  echo -e "\nINSTALLING ZSH HANDLER" &&\
-  sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" 
-}
-
-# here it starts
-init
-
+main
--- script_for_archinstall.sh
+++ script_for_archinstall.sh
@@ -1,19 +1,20 @@
 #! /usr/bin/bash
-function init(){
-  setImportantPrograms &&\
-  getConfigFiles &&\
-  cleanPreviousConfigs &&\
-  setConfigFiles 
+function init() {
+  setupXdgDirectories &&
+    getConfigFiles &&
+    cleanPreviousConfigs &&
+    setImportantPrograms &&
+    cleanPreviousConfigs &&
+    setConfigFiles
 }
-function setImportantPrograms(){
-    setupXdgDirectories&&\
-    setupReflector &&\
-    setupDocker &&\
-    setupZsh &\
-    setupPyenv &\
-    setupNvm &
+function setImportantPrograms() {
+  setupReflector &&
+    setupDocker &&
+    setupZsh &
+  setupPyenv &
+  setupNvm &
 }
-function cleanPreviousConfigs(){
+function cleanPreviousConfigs() {
   rm -rf $HOME/.vim
   rm -rf $HOME/.tmux.conf
   rm -rf $HOME/.zshrc
@@ -22,74 +23,68 @@ function cleanPreviousConfigs(){
   rm -rf $HOME/.config/alacritty
   rm -rf $HOME/.config/i3
 }
-function setupXdgDirectories(){
-  if ! command -v xdg-user-dirs 2>&1 > /dev/null
-  then 
-  pacman -S xdg-user-dirs
+function setupXdgDirectories() {
+  if ! command -v xdg-user-dirs 2>&1 >/dev/null; then
+    pacman -S xdg-user-dirs
   fi
-  xdg-user-dirs-update 
+  xdg-user-dirs-update
 }
-function setupReflector(){
-  if ! command -v reflector 2>&1 >/dev/null
-  then
-     pacman -S reflector
+function setupReflector() {
+  if ! command -v reflector 2>&1 >/dev/null; then
+    pacman -S reflector
   fi
-   systemctl enable reflector.timer
+  systemctl enable reflector.timer
 }
-function setupDocker(){
-  
-  if ! command -v docker 2>&1 >/dev/null
-  then
-     pacman -S docker
+function setupDocker() {
+
+  if ! command -v docker 2>&1 >/dev/null; then
+    pacman -S docker
   fi
- systemctl enable docker.service &&  usermod -aG docker $USER && newgrp docker
+  systemctl enable docker.service && usermod -aG docker $USER && newgrp docker
 }
-function setZsh(){
-  echo -e "\nINSTALLING ZSH HANDLER" &&\
-  sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" 
+function setZsh() {
+  echo -e "\nINSTALLING ZSH HANDLER" &&
+    sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 }
-function setupPyenv(){
-  curl https://pyenv.run | bash &&\
-  echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-  echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-  echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+function setupPyenv() {
+  curl https://pyenv.run | bash &&
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >>~/.zshrc
+  echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >>~/.zshrc
+  echo 'eval "$(pyenv init -)"' >>~/.zshrc
 }
-function setupNvm(){
+function setupNvm() {
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 }
-function getConfigFiles(){
-  if ! command -v stow 2>&1 >/dev/null
-  then
-     pacman -S stow
+function getConfigFiles() {
+  if ! command -v stow 2>&1 >/dev/null; then
+    pacman -S stow
   fi
-  if ! command -v git 2>&1 >/dev/null
-  then
-   pacman -S git
+  if ! command -v git 2>&1 >/dev/null; then
+    pacman -S git
   fi
-  git clone https://github.com/juanpabloinformatica/dotfiles.git $HOME/Documents/dotfiles &&\
-  cd $HOME/Documents/dotfiles
-  stow --target=$HOME home &&\
-  stow --target=$HOME/Pictures pictures
-  stow --target=$HOME/.config /config 
+  git clone https://github.com/juanpabloinformatica/dotfiles.git $HOME/Documents/dotfiles &&
+    cd $HOME/Documents/dotfiles
+  stow --target=$HOME home &&
+    stow --target=$HOME/Pictures pictures
+  stow --target=$HOME/.config /config
   cd $PWD
 }
 
-function setConfigFiles(){
-  setFont &\
+function setConfigFiles() {
+  setFont &
   setAlacritty
 }
-function setFont(){
+function setFont() {
   echo -e "\nINSTALLING Iosevka FONT"
-  mkdir -p $HOME/.local/share/fonts &&\
- wget --directory-prefix $HOME/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Iosevka.zip &&\
-  unzip $HOME/.local/share/fonts/Iosevka.zip -d $HOME/.local/share/fonts/Iosevka.zip &&\
- rm -rf Iosevka.zip
+  mkdir -p "$HOME"/.local/share/fonts &&
+    wget --directory-prefix $HOME/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Iosevka.zip &&
+    unzip "$HOME"/.local/share/fonts/Iosevka.zip -d $HOME/.local/share/fonts/Iosevka.zip &&
+    rm -rf Iosevka.zip
 }
-function setAlacritty(){
+function setAlacritty() {
   echo -e "\n GETTING Alacritty THEMES"
   git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
 }
 
 # here it starts
 init
-
